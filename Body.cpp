7b2e5d93b3ac52371c6e846dba5430d7a3683e3f
Vladlen <ilusenkovladlen@gmail.com>
2021-04-17 12:02:33 +0300

    Создан класс Body, написаны некоторые методы для него

---
+++ Body.cpp
@@ -0,0 +1,50 @@
+#include "Body.h"
+
+long Body::getNumberOfPixels() const{
+    return width*depth;
+}
+
+void Body::createADataArray(){
+    long size = this->getNumberOfPixels();
+    Body::data = new Pixel[size];
+}
+
+//Чтение из файла
+void Body::readAFile(string address) {
+    //Создаем массив, в который будем заносить информацию о пикселях
+    this->createADataArray();
+
+    //Открытие файла и пропуск Header'а
+    ifstream file (address, ios::in | ios::binary);
+    file.seekg(432);
+
+    //Поочередно обрабатываем каждый пиксель
+    long proceeded_pixels = 0;
+    for (int counter = 0; counter < depth; ++counter) {
+        //TODO: каким-то образом передать параметр file, или убрать метод (предпочтиельно первое)
+        this->readAline(file, proceeded_pixels);
+    }
+}
+
+//TODO: Возможно, стоит обьединить с readAFile
+void Body::readAline(ifstream file, long &proceeded_pixels){
+    for (int counter = 0; counter < width; ++counter) {
+        //Чтение цвета пикселя
+        int8_t red;
+        file.read((char*)&red, sizeof(int8_t));
+        int8_t green;
+        file.read((char*)&green, sizeof(int8_t));
+        int8_t blue;
+        file.read((char*)&blue, sizeof(int8_t));
+
+        //Запись информации о пикселе в массив
+        data[proceeded_pixels] = Pixel(red, green, blue);
+        proceeded_pixels++;
+    }
+    //TODO: скип нулевых байтов
+}
+
+//Увеличение массива пикселей в заданное количество раз
+void Body::enlargeImage(int coeff) {
+
+}
---
+++ Body.h
@@ -0,0 +1,31 @@
+#include "Pixel.h"
+#include <string>
+#include <fstream>
+
+class Body {
+private:
+    //Атрибуты
+    Pixel *data;
+    int32_t width;
+    int32_t depth;
+
+    //Методы
+    long getNumberOfPixels() const;
+
+    void readAline(ifstream file, long &proceeded_pixels);
+
+    void createADataArray();
+public:
+    Body(int32_t width, int32_t depth){
+        Body::width = width;
+        Body::depth = depth;
+    }
+
+    void readAFile(string address);
+
+    void enlargeImage(int coeff);
+
+
+
+};
+
